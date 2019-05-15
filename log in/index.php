db5f1a1be54e8a6fb8c578f041fc6859c4c33b8a
yitaodong <47155310+yitaodong@users.noreply.github.com>
2019-05-15 16:36:08 -0400

    Add files via upload

---
+++ log in/index.php
@@ -0,0 +1,23 @@
+<?php
+echo "<<html>\n";
+echo "   <head>\n";
+echo "       <title>Login </title>\n";
+echo "       <link rel=\"stylesheet\" type=\"text/css\" href=\"style1.css\"\n";
+echo "       <body>\n";
+echo "          <div class=\"loginbox\">\n";
+echo "          <img src=\"head.jpg\" class=\"head\">\n";
+echo "          <h1>Login here</h1>\n";
+echo "          <from>\n";
+echo "              <p>Username</p>\n";
+echo "              <input type=\"text\" name=\"\" placeholder=\"Enter Username\">\n";
+echo "              <p>Password</p>\n";
+echo "              <input type=\"password\" name=\"\" placeholder=\"Enter Password\">\n";
+echo "              <input type=\"submit\" name=\"\" value=\"Login\">\n";
+echo "              <a href=\"#\">Lost your password?</a><br>\n";
+echo "              <a href=\"#\">Don't have an account</a>\n";
+echo "          </from>\n";
+echo "          </div>\n";
+echo "       </body>\n";
+echo "   </head>\n";
+echo "/html>\n";
+?>
