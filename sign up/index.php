685749a6d7507bc8938371dbc075fc38d4df30bd
yitaodong <47155310+yitaodong@users.noreply.github.com>
2019-05-15 16:36:25 -0400

    Add files via upload

---
+++ sign up/index.php
@@ -0,0 +1,28 @@
+<?php
+echo "<html>\n";
+echo "head>\n";
+echo "title> Sign Up </title>\n";
+echo "   <link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\">\n";
+echo "/head>\n";
+echo "body>\n";
+echo "    <div id=\"login-box\">\n";
+echo "       <div class=\"left-box\">\n";
+echo "         <h1> Sign Up</h1>\n";
+echo "          \n";
+echo "          <input type=\"text\" name=\"username\" placeholder=\"Username\"/>\n";
+echo "          <input type=\"text\" name=\"email\" placeholder=\"Email\"/>\n";
+echo "          <input type=\"password\" name=\"password\" placeholder=\"password\"/>\n";
+echo "          <input type=\"password\" name=\"password2\" placeholder=\"Retype password\"/>\n";
+echo "           <input type=\"submit\" name=\"signup-button\" value=\"Sign Up\"/>\n";
+echo "       </div>\n";
+echo "       <div class=\"right-box\">\n";
+echo "          <span class=\"signinwith\"> Sign in with<br/> Social Network\n";
+echo "          </span>\n";
+echo "         <button class=\"social facebook\">Log in with Facebook</button>\n";
+echo "        <button class=\"social google\">Log in with Google+</button>\n";
+echo "       </div>\n";
+echo "       <div class=\"or\"> OR</div>\n";
+echo "    </div>\n";
+echo "  </body>\n";
+echo "/html>\n";
+?>
